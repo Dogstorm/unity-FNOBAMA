11658b2ff41cd99663af1aa005f83776a82a1d15
Dogstorm <natelittl3@gmail.com>
2023-06-25 02:30:06 -0700

    change#5

--- Five Nights At Obama's/Assets/Scripts/DoorButton.cs
+++ Five Nights At Obama's/Assets/Scripts/DoorButton.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class DoorButton : MonoBehaviour
 {
+    public bool toggle;
+    public GameObject Door;
     // Start is called before the first frame update
     void Start()
     {
-        
+        toggle = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            if (toggle == false)
+            {
+                Door.SetActive(false);
+                toggle = true;
+                Debug.Log("true");
+            }
+            else if (toggle == true)
+            {
+                Door.SetActive(true);
+                toggle = false;
+            }
+            
+           
+            }
         
-    }
+        }
 }
+
--- Five Nights At Obama's/Assets/Scripts/PlayerMovement.cs
+++ Five Nights At Obama's/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     [Header("Movement")]
     public float moveSpeed;
 
@@ -37,8 +35,8 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
-        
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f,whatIsGround);
+
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
         if (grounded)
         {
             rb.drag = groundDrag;
@@ -68,7 +66,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-        if(flatVel.magnitude > moveSpeed)
+        if (flatVel.magnitude > moveSpeed)
         {
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
