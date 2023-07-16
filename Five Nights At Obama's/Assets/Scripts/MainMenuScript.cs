981a37aa1a772a083c6ff1fce81cd589b00c32cf
Oconop23 <121410863+Oconop23@users.noreply.github.com>
2023-07-16 11:38:32 -0700

    Added Main Menu Screen

--- Five Nights At Obama's/Assets/Scripts/MainMenuScript.cs
+++ Five Nights At Obama's/Assets/Scripts/MainMenuScript.cs
@@ -7,6 +7,12 @@ public class MainMenuScript : MonoBehaviour
 {
     public void StartGame ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("User Quit The Game");
+        Application.Quit();
     }
 }
