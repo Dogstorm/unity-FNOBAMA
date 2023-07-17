d06cc99306fa8c3ee8c5a8e862ec340bb48f8f32
Oconop23 <121410863+Oconop23@users.noreply.github.com>
2023-07-16 23:14:46 -0700

    Sense Adjusting scripts

--- Five Nights At Obama's/Assets/Scripts/SensAdjust.cs
+++ Five Nights At Obama's/Assets/Scripts/SensAdjust.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class SensAdjust : MonoBehaviour
 {
     public GameObject mainCam;
-    public float xSense;
-    public float ySense;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float xSense { get; set; }
+    public float ySense { get; set; }
+    string Sensekey = "Sense";
+    
     // Update is called once per frame
     void Update()
     {
         xSense = mainCam.GetComponent<PlayerCam>().sensX;
         ySense = mainCam.GetComponent<PlayerCam>().sensY;
+    }
 
+    private void Awake()
+    {
+        xSense = PlayerPrefs.GetFloat(Sensekey);
+    }
 
+    public void SetSense(float sense)
+    {
+        PlayerPrefs.SetFloat(Sensekey, sense);
     }
 }
