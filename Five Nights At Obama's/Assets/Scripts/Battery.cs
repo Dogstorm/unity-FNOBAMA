98187595ba71dd035ee5e936e54b41d6ede94948
Oconop23 <121410863+Oconop23@users.noreply.github.com>
2023-07-18 15:19:43 -0700

    trying to fix battery

--- Five Nights At Obama's/Assets/Scripts/Battery.cs
+++ Five Nights At Obama's/Assets/Scripts/Battery.cs
@@ -3,24 +3,39 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Battery : MonoBehaviour
 {
     public bool drainOverTime;
-    public float drainRate = 0.001f;
-    public float batteryLevel = 100;
-    public TMP_Text batteryText;
+    [SerializeField] 
+    private float drainRate = 0.001f;
+    [SerializeField]
+    private float batteryLevel = 100f;
+    [SerializeField]
+    private TMP_Text batteryText;
     void Start()
     {
-        
+       startDrain();
+    }
+
+    public void startDrain ()
+    {
+        Debug.Log("draining battery");
+      
     }
 
     void Update()
     {
-        batteryText.text = string.Format(batteryLevel.ToString("0"));
-        if(drainOverTime == true)
+        if (drainOverTime == true)
         {
+            
+            
+            //float realBattery = batteryLevel / 100;
             batteryLevel -= drainRate;
+            batteryLevel = Mathf.Round(batteryLevel * 100f) / 10000f;
+            batteryText.text = batteryLevel + "%";
         }
+        
     }
 }
