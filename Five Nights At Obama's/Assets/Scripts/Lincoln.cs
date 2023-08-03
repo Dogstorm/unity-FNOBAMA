588ddb72f3c2b7319080fd6adff629920a7c2a85
Oconop23 <121410863+Oconop23@users.noreply.github.com>
2023-08-02 21:50:17 -0700

    some scene changes and fixes

---
+++ Five Nights At Obama's/Assets/Scripts/Lincoln.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lincoln : MonoBehaviour
+{
+    public GameObject reagan;
+    public float randomMove;
+    public float randomtimetoMove;
+    public float rate = 0.001f;
+    public GameObject spot;
+    public GameObject spot1;
+    public GameObject spot2;
+    void Start()
+    {
+        randomtimetoMove = 0;
+        
+    }
+    void randomGenerate()
+    {
+        randomMove = Random.Range(0, 3);
+        Debug.Log("GeneratedRandomPos");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(randomtimetoMove <= 0)
+        {
+            randomtimetoMove = Random.Range(120, 320);
+            randomGenerate();
+        }
+        if(randomtimetoMove > 0)
+        {
+            randomtimetoMove -= rate;
+        }
+
+        if(randomMove == 0)
+        {
+            reagan.transform.position = spot.transform.position;
+        }
+
+        if (randomMove == 1)
+        {
+            reagan.transform.position = spot1.transform.position;
+        }
+
+        if (randomMove == 2)
+        {
+            reagan.transform.position = spot2.transform.position;
+        }
+    }
+}
